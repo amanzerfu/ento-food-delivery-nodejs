5f9d70a6221704a2af66f161852d0f1f6dafbf1e
Amanuel Zerfu <82865211+amanzerfu@users.noreply.github.com>
2024-08-05 13:10:13 +0300

    Update db.js
    conncetion string

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,6 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: postgresql://ento_delivery_user:lVmFvXRWyTpMIaELPW8DfAaK5FV8oPQw@dpg-cqnt705ds78s73boips0-a/ento_delivery_user_db,
-});
+  connectionString: process.env.DATABASE_URL});
 
 module.exports = pool;
